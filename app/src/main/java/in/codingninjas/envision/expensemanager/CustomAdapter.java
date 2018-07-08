bd1dcc9dab10049cb629c49492dc24c1ac54bbd2
Vidit Jain <vidit@MacBook-Air-2.local>
2018-07-08 14:31:12 +0530

    Completed CustomAdapter

---
+++ app/src/main/java/in/codingninjas/envision/expensemanager/CustomAdapter.java
@@ -0,0 +1,121 @@
+//Vidit Jain
+package in.codingninjas.envision.expensemanager;
+
+import android.content.Context;
+import android.preference.PreferenceActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+
+    Context mContext;
+    ArrayList<Item> itemsList;
+    public LayoutInflater inflater;
+
+    // Constructor
+    public CustomAdapter(Context context, ArrayList<Item> itemsList){
+        super(context,0,itemsList);
+        mContext = context;
+        inflater=(LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.itemsList = itemsList;
+    }
+
+    // This function return the number of different type of views that will be there in the list view.
+    @Override
+    public int getViewTypeCount()
+    {
+        return 2;
+    }
+
+    // This function returns the type of item(in our case header or list item) that adapter wants to know in getView function.
+    @Override
+    public int getItemViewType(int position)
+    {
+        int type=itemsList.get(position).getItemType();
+        return type;
+    }
+
+    // This function gives the total count of items that will be there in the list.
+    @Override
+    public int getCount()
+    {
+        return itemsList.size();
+    }
+
+    // This function returns the object of the itemList that has to inflated at that position.
+    @Override
+    public Object getItem(int position)
+    {
+        return itemsList.get(position);
+    }
+
+
+    // This function returns the unique id associated with every inflated layout, since it's is not useful in our case so
+    // we return the position, which is also unique for every item.
+    @Override
+    public long getItemId(int position)
+    {
+        return position;
+    }
+
+
+
+
+    // This is the function in which we have to inflate the layout as per its TYPE
+    // this function gets the type of each item from getItemViewType and on the basis of it we apply if else and inflate the layout.
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        View output=convertView;
+        int type=getItemViewType(position);
+        if(output==null)
+        {
+            if(type==0)
+            {
+                output=inflater.inflate(R.layout.header_item_layout,parent,false);
+                TextView headerTitleTextView=output.findViewById(R.id.headerTitleTextView);
+                HeaderItemViewHolder headerItemViewHolder=new HeaderItemViewHolder(headerTitleTextView);
+                output.setTag(headerItemViewHolder);
+            }
+            if(type==1)
+            {
+                output=inflater.inflate(R.layout.expense_row_layout,parent,false);
+                TextView expenseName=output.findViewById(R.id.expenseName);
+                TextView expenseAmount=output.findViewById(R.id.expenseAmount);
+                TextView expenseDate=output.findViewById(R.id.expenseDate);
+                TextView expenseTime=output.findViewById(R.id.expenseTime);
+                ExpenseViewHolder viewHolder=new ExpenseViewHolder();
+                viewHolder.title=expenseName;
+                viewHolder.amount=expenseAmount;
+                viewHolder.date=expenseDate;
+                viewHolder.time=expenseTime;
+                output.setTag(viewHolder);
+            }
+        }
+        if(type==0)
+        {
+            HeaderItem headerItem=(HeaderItem) itemsList.get(position);
+            HeaderItemViewHolder headerItemViewHolder=(HeaderItemViewHolder) output.getTag();
+            headerItemViewHolder.headerTitleTextView.setText(headerItem.getHeaderTitle());
+        }
+        if(type==1)
+        {
+            Expense expense=(Expense) itemsList.get(position);
+            ExpenseViewHolder viewHolder=(ExpenseViewHolder) output.getTag();
+            viewHolder.title.setText(expense.getName());
+            viewHolder.amount.setText(String.valueOf(expense.getAmount()));
+            viewHolder.date.setText(expense.getDate());
+            viewHolder.time.setText(expense.getTime());
+        }
+        return output;
+    }
+
+
+}
